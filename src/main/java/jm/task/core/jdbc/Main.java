7bf9e15d6fe1e5472ddccfd4e99ab29831e8da9c
Евгений <jekarundin@yandex.ru>
2022-12-12 22:25:00 +0600

    54321 у-а-а-а-а-а

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Sergo","Billionaire", (byte) 28);
+        userService.saveUser("Michel","Pivozavr", (byte) 30);
+        userService.saveUser("Gosha","Frontovik", (byte) 21);
+        userService.saveUser("Andrey","Comporator", (byte) 54);
+        List<User> user = userService.getAllUsers();
+        for (User person: user) {
+            System.out.println(person);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
