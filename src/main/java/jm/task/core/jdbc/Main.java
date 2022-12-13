7a9fe42b00ca81bc5925acbb08f4b0491144c8a9
Евгений <jekarundin@yandex.ru>
2022-12-13 21:41:43 +0600

    Frankenstein

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,6 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,10 +11,8 @@ public class Main {
         userService.saveUser("Michel","Pivozavr", (byte) 30);
         userService.saveUser("Gosha","Frontovik", (byte) 21);
         userService.saveUser("Andrey","Comporator", (byte) 54);
-        List<User> user = userService.getAllUsers();
-        for (User person: user) {
-            System.out.println(person);
-        }
+
+        System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,27 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
-    private static final String url = "jdbc:postgresql://127.0.0.1:5432/postgres";
-    private static final String log = "postgres";
-    private static final String pass = "20061993";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "20061993";
+    private static final String DRIVER_CLASS = "org.postgresql.Driver";
+    private static final String DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
 
-    public static Connection getConnectionDataBase() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url, log, pass);
-            if (connection != null) {
-                System.out.println("База данных подключена!");
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 
-        return connection;
+    public static SessionFactory getSessionFactory() {
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.driver_class", DRIVER_CLASS);
+        properties.setProperty("hibernate.connection.url", URL);
+        properties.setProperty("hibernate.connection.username", USERNAME);
+        properties.setProperty("hibernate.connection.password", PASSWORD);
+        properties.setProperty("hibernate.dialect", DIALECT);
+
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(User.class)
+                .setProperties(properties);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
     }
 }
